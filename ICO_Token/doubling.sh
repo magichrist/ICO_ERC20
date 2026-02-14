aeb7131da5f2123c57c3756c1e7e63eaaa143aaa
magichrist <alitabaee2002@gmail.com>
2026-02-14 23:28:53 +0330

    doubling now works

--- ICO_Token/doubling.sh
+++ ICO_Token/doubling.sh
@@ -1,2 +1,3 @@
+source .env
 echo "Doubling() was called" #must be called once a week (604_000 seconds based on contract)
 cast send $CA "Doubling()()" --rpc-url=$ETH_HOODI2 --private-key=$PriTestDeply
